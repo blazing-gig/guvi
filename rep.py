e431287376575167f882ab5c7f23e47177d16089
vincorp-adi <gojeta.aditya@gmail.com>
2016-07-08 22:33:25 +0530

    Add files via upload

---
+++ rep.py
@@ -0,0 +1,20 @@
+n = int(raw_input())
+l = []
+
+l = map(int, raw_input().split())
+if len(l) > n:
+	print "input too long"
+elif len(l) == 1:
+	print 0
+else:
+	flag = 0
+	d = dict.fromkeys(l,0)
+	for key in l:
+		d[key] = d[key] + 1
+	print "repeated numbers are "
+	for key in d.keys():
+		if d[key] > 1:
+			print key
+			flag = 1
+	if flag == 0:
+		print 0
